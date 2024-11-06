71c2cec3a85647de8c59e41970bf808dbe8d3437
Will Mear <wxm158@student.bham.ac.uk>
2024-11-06 18:00:52 +0000

    initial commit

---
+++ src/test/java/com/willmear/OAuth2Login/OAuth2LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willmear.OAuth2Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
